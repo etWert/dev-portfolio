2916d341fa995eaf7625a0766dde176e0d8ed8ea
etWert <i6725155@gmail.com>
2025-03-04 19:22:23 +0200

    Add Header component with styling

---
+++ dev-portfolio-client/src/components/header/Header.js
@@ -0,0 +1,18 @@
+import "./header.css"
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-container">
+        <img className="logo" src="logo.svg" alt="Logo" />
+        <nav className="header-links">
+          <a href="#about" className="">אודות</a>
+          <a href="#skills" className="">מיומנויות</a>
+          <a href="#projects" className="">פרויקטים</a>
+          <a href="#contact" className="">צור קשר</a>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;

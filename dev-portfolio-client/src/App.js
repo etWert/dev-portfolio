d9b71b20acb369d0adaa1388f8e647a5f4bfa9fa
etWert <i6725155@gmail.com>
2025-03-17 18:23:43 +0200

    Add Temp component

--- dev-portfolio-client/src/App.js
+++ dev-portfolio-client/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css"
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
+import Temp from "./components/temp/Temp";
 
 const App = () => {
     return (
         <>
             <Header />
             <Main />
+            <Temp/>
         </>
     );
 }
---
+++ dev-portfolio-client/src/components/temp/Temp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './temp.css';
+
+const Temp = () => {
+    return (
+        <div className="temp-container">
+            <h2>Website Under Development</h2>
+            <p>
+                Exciting updates are on the way—stay tuned!
+            </p>
+        </div>
+    );
+};
+
+export default Temp;
